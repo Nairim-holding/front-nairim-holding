15e477ad47b7d9eff246dfedbfa5c9156533e5e4
Marcio <marciopivajunior457@gmail.com>
2025-05-18 01:52:29 -0300

    Feat: finalizando a estruturação do aside e darkmode

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import Aside from '@/components/Admin/Aside';
 
 export default function Page(){
     const [name, setName] = useState<string>(' matheus henrique dos santos bino ');
+    const [darkMode, setDarkMode] = useState<boolean>(false);
     
     useEffect(() => {
         const cookie = Cookies.get('authToken') as string;
@@ -15,6 +16,6 @@ export default function Page(){
     }, [])
 
     return(
-        <Aside></Aside>
+        <Aside darkMode={darkMode} setDarkMode={setDarkMode} ></Aside>
     )
 }
---
+++ src/components/Admin/Aside/List/index.tsx
@@ -0,0 +1,125 @@
+import { CiHeart } from "react-icons/ci";
+import { RxDashboard } from "react-icons/rx";
+import { LuUserRound } from "react-icons/lu";
+import { TbDeviceAnalytics, TbMessage } from "react-icons/tb";
+import { IoFileTrayFullOutline } from "react-icons/io5";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { CiSettings } from "react-icons/ci";
+import Link from "next/link";
+import PropsDarkMode from "@/types/propsDarkMode";
+import ButtonToggle from "../ButtonToggle";
+
+interface PropsButtonToggle extends PropsDarkMode {
+  openAside: boolean;
+}
+
+export default function List({
+  darkMode,
+  setDarkMode,
+  openAside,
+}: PropsButtonToggle) {
+  return (
+    <ul className="flex flex-col gap-5 items-start justify-start w-full">
+      <Link
+        href="#"
+        className={`mt-5 ${
+          openAside ? "w-auto" : "w-full"
+        } flex gap-3 justify-center`}>
+        <RxDashboard size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>
+            Dashboard
+          </p>
+        )}
+      </Link>
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <LuUserRound size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>User</p>
+        )}
+      </Link>
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <TbMessage size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>Message</p>
+        )}
+      </Link>
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <TbDeviceAnalytics size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>
+            Analytics
+          </p>
+        )}
+      </Link>
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <IoFileTrayFullOutline
+          size={25}
+          color={`${darkMode ? "#fff" : "#000"}`}
+        />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>
+            File Manager
+          </p>
+        )}
+      </Link>
+
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <AiOutlineShoppingCart
+          size={25}
+          color={`${darkMode ? "#fff" : "#000"}`}
+        />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>Order</p>
+        )}
+      </Link>
+
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <CiHeart size={30} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>Saved</p>
+        )}
+      </Link>
+
+      <Link
+        href="#"
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3`}>
+        <CiSettings size={30} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>Setting</p>
+        )}
+      </Link>
+
+      <ButtonToggle
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        openAside={openAside}></ButtonToggle>
+    </ul>
+  );
+}
