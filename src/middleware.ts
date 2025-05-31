7ed94ff6ec79885eb8cc99ee4f22c7df4f20640b
Marcio <marciopivajunior457@gmail.com>
2025-05-30 21:10:14 -0300

    Feat: adicionado verificação jwt no middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,30 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export default function middleware(req: NextRequest){
+interface ResVerifyToken {
+    status: number;
+    message: string;
+}
+
+export default async function middleware(req: NextRequest){
     const token = req.cookies.get('authToken');
 
     if(!token){
         return NextResponse.redirect(new URL('/', req.url))
     }
 
-    //verificar o token é valido no back
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/authenticate/verify`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: token.value })
+    });
+
+    const tokenIsValid = await response.json() as ResVerifyToken;
+    
+    if(tokenIsValid.status == 401 || tokenIsValid.status == 400){
+        return NextResponse.redirect(new URL('/', req.url))
+    }
 
     return NextResponse.next()
 }
