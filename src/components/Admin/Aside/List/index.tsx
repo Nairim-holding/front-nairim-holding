e3b62e99ef9befb2cda8e254ee113638c1b8888d
Luan Dos Santos <muchasgraccias@gmail.com>
2025-06-04 23:07:26 -0300

    Feat: aside refatorado

--- src/components/Admin/Aside/List/index.tsx
+++ src/components/Admin/Aside/List/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
 import { RxDashboard } from "react-icons/rx";
 import { LuUserRound } from "react-icons/lu";
+import { MdOutlineHomeWork } from "react-icons/md";
 import { TbDeviceAnalytics, TbMessage } from "react-icons/tb";
 import { IoFileTrayFullOutline } from "react-icons/io5";
 import { AiOutlineShoppingCart } from "react-icons/ai";
@@ -18,6 +20,8 @@ export default function List({
   setDarkMode,
   openAside,
 }: PropsButtonToggle) {
+  const [showSublist, setShowSublist] = useState(false);
+  
   return (
     <ul className="flex flex-col gap-5 items-start justify-start w-full">
       <Link
@@ -32,79 +36,57 @@ export default function List({
           </p>
         )}
       </Link>
-      <Link
-        href="/dashboard/signup"
-        className={`${
-          openAside ? "w-auto" : "w-full"
-        } flex justify-center gap-3`}>
-        <LuUserRound size={25} color={`${darkMode ? "#fff" : "#000"}`} />
-        {openAside && (
-          <p className={`${darkMode ? "text-white" : "text-black"}`}>User</p>
-        )}
-      </Link>
-      <Link
-        href="#"
-        className={`${
-          openAside ? "w-auto" : "w-full"
-        } flex justify-center gap-3`}>
-        <TbMessage size={25} color={`${darkMode ? "#fff" : "#000"}`} />
-        {openAside && (
-          <p className={`${darkMode ? "text-white" : "text-black"}`}>Message</p>
-        )}
-      </Link>
-      <Link
-        href="#"
-        className={`${
-          openAside ? "w-auto" : "w-full"
-        } flex justify-center gap-3`}>
-        <TbDeviceAnalytics size={25} color={`${darkMode ? "#fff" : "#000"}`} />
-        {openAside && (
-          <p className={`${darkMode ? "text-white" : "text-black"}`}>
-            Analytics
-          </p>
-        )}
-      </Link>
-      <Link
-        href="#"
-        className={`${
-          openAside ? "w-auto" : "w-full"
-        } flex justify-center gap-3`}>
-        <IoFileTrayFullOutline
-          size={25}
-          color={`${darkMode ? "#fff" : "#000"}`}
-        />
-        {openAside && (
-          <p className={`${darkMode ? "text-white" : "text-black"}`}>
-            File Manager
-          </p>
-        )}
-      </Link>
 
+      {/* Cadastrar com sublista */}
+      <div
+        onClick={() => setShowSublist(!showSublist)}
+        className={`${openAside ? "w-auto" : "w-full"} cursor-pointer flex flex-col gap-2`}
+      >
+        <p
+        className={`${
+          openAside ? "w-auto" : "w-full"
+        } flex justify-center gap-3 mr-10`}>
+        <MdOutlineHomeWork size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+        {openAside && (
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>Cadastrar</p>
+        )}
+      </p>
+
+        {/* Sublista */}
+        {openAside && showSublist && (
+          <ul className="flex flex-col gap-2">
+            <li className="flex gap-5 flex-row ml-5 items-center">
+              <MdOutlineHomeWork size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+              <Link href="/dashboard/signup/clientes" className="text-sm text-gray-500 hover:text-blue-500">
+                <p className={`${darkMode ? "text-white" : "text-black"}`}>Usuario</p>
+              </Link>
+            </li>
+            <li className="flex gap-5 flex-row ml-5 items-center">
+              <MdOutlineHomeWork size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+              <Link href="/dashboard/signup/produtos" className="text-sm text-gray-500 hover:text-blue-500">
+                <p className={`${darkMode ? "text-white" : "text-black"}`}>Imobiliária</p>
+              </Link>
+            </li>
+            <li className="flex gap-5 flex-row ml-5 items-center">
+              <MdOutlineHomeWork size={25} color={`${darkMode ? "#fff" : "#000"}`} />
+              <Link href="/dashboard/signup/pedidos" className="text-sm text-gray-500 hover:text-blue-500">
+                <p className={`${darkMode ? "text-white" : "text-black"}`}>Cadastrar</p>
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
+      
       <Link
         href="#"
         className={`${
           openAside ? "w-auto" : "w-full"
         } flex justify-center gap-3`}>
-        <AiOutlineShoppingCart
-          size={25}
-          color={`${darkMode ? "#fff" : "#000"}`}
-        />
+        <RxDashboard size={25} color={`${darkMode ? "#fff" : "#000"}`} />
         {openAside && (
-          <p className={`${darkMode ? "text-white" : "text-black"}`}>Order</p>
+          <p className={`${darkMode ? "text-white" : "text-black"}`}>Resumo</p>
         )}
       </Link>
-
-      <Link
-        href="#"
-        className={`${
-          openAside ? "w-auto" : "w-full"
-        } flex justify-center gap-3`}>
-        <CiHeart size={30} color={`${darkMode ? "#fff" : "#000"}`} />
-        {openAside && (
-          <p className={`${darkMode ? "text-white" : "text-black"}`}>Saved</p>
-        )}
-      </Link>
-
       <Link
         href="#"
         className={`${
