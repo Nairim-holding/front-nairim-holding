7db31df795a667230575466647c348b747f2d66e
Marcio <marciopivajunior457@gmail.com>
2025-10-04 20:33:30 -0300

    Fix: pagina de configuração

--- src/app/dashboard/configuracoes/page.tsx
+++ src/app/dashboard/configuracoes/page.tsx
@@ -4,8 +4,8 @@ export default function Page(){
     return(
         <center>
             <Image src={'/brainrot/gif1.gif'} width={500} height={500} alt="abatukan"></Image>
-            <h1 className="font-bold">O cara acha que vou fazer a página de configuração</h1>
-            <h2>Não sei centralizar uma div por isso irei usar {`<center>`} - levei vantagi</h2>
+            <h1 className="font-bold text-[100px]"></h1>
+            <h2>Eu sou o maior fazedor de páginas de configurações da Nairim Holding. Eu amo fazer página de configuração. Eu amo configurar tudo. Vou bater o recorde mundial em fazer páginas de configuração. 2500 páginas pra eu fazer. 2500 páginas de configurações entregues. Eu faço página pros caras. Eu sou um fazedor de páginas de configurações. Nairim Holding, eu estou pedindo a vocês, venham me deixar fazer mais páginas de configuração.</h2>
         </center>
     )
 }
